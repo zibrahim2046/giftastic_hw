e26669cacd5aee73235112c5e628e3fa80e6fd38
zibrahim2046 <zibrahim2046@gmail.com>
2020-03-06 15:58:33 -0600

    added function to add rating to image, and to append the returned images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,39 @@
 $(document).ready(function() {
+  var displayedButtons = ["Naruto", "Sasuke", "Kakashi"];
 
-var displayedButtons = ["Naruto", "Sasuke", "Kakashi"];
-
-function displayImg(){
+  function displayImg() {
     $("display-images").empty();
     var input = $(this).attr("data-name");
     var limit = 10;
-    var queryURL= "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=" + limit + "&api_key=J6LoY96gEICI3KAnOxmy19HJGwWNQ6tm";
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).done(function(response) {
-
-    for(var i = 0; i<limit; i++) {
-
+    var queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      input +
+      "&limit=" +
+      limit +
+      "&api_key=J6LoY96gEICI3KAnOxmy19HJGwWNQ6tm";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      for (var i = 0; i < limit; i++) {
         var displayDiv = $("<div>");
         displayDiv.addClass("holder");
 
         var image = $("<img>");
         image.attr("src", response.data[i].images.original_still.url);
-    }
-}
+        image.attr("data-still", response.data[i].images.original_still.url);
+        image.attr("data-animate", response.data[i].images.original.url);
+        image.attr("data-state", "still");
+        image.attr("class", "gif");
+        displayDiv.append(image);
 
+        var rating = response.data[i].rating;
+        console.log(response);
+        var pRating = $("<p>").text("Rating: " + rating);
+        displayDiv.append(pRating);
 
-}
-
-
-}
+        $("display-images").append(displayDiv);
+      }
+    });
+  }
+});
